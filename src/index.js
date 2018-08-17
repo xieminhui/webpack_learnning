753838588d1f73ba65569d5ca2815d8c74c03636
xieminhui <xmh19940228@163.com>
2018-08-17 17:45:01 +0800

    webpack learnning 4 ---- webpack output

--- src/index.js
+++ src/index.js
@@ -1,23 +1,14 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
+import printMe  from './print.js';
 
 function component() {
    var element = document.createElement('div');
+   let btn = document.createElement('button');
 
-   // Lodash, currently included via a script, is required for this line to work
-   // Lodash, now imported by this script
    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-   element.classList.add('hello');
-
-   //add img
-   let myIcon = new Image();
-   myIcon.src = Icon;
-
-   element.appendChild(myIcon);
-
-   console.log(Data);
+   btn.innerHTML = 'Click me and check the console!';
+   btn.onclick = printMe;
+   element.appendChild(btn);
 
    return element;
 }
