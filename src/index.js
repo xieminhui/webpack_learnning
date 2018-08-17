98b8601ec6841d13b6349a6b45578a85b3b9c91b
xieminhui <xmh19940228@163.com>
2018-08-17 15:03:35 +0800

    webpack step 3 --- webpack loader

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png';
+import Data from './data.xml';
+
 function component() {
-   let ele = document.createElement('div');
-   ele.innerHTML = _.join(['hello', 'webpack'], '');
-   return ele;
+   var element = document.createElement('div');
+
+   // Lodash, currently included via a script, is required for this line to work
+   // Lodash, now imported by this script
+   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+   element.classList.add('hello');
+
+   //add img
+   let myIcon = new Image();
+   myIcon.src = Icon;
+
+   element.appendChild(myIcon);
+
+   console.log(Data);
+
+   return element;
 }
+
 document.body.appendChild(component());
