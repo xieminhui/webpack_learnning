4332010efc2fd2f0fa7cdfebe86e19940ced688a
xieminhui <xmh19940228@163.com>
2018-08-22 16:49:34 +0800

    webpack learnning ------ hot replacement

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,60 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
    entry: {
-      app: './src/index.js',
-      print: './src/print.js'
+      app: './src/index.js'
    },
    output: {
       filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist')
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/',
    },
+   devtool: 'inline-source-map',
+   mode: 'production',
    plugins: [
       new HtmlWebpackPlugin({
-         title: 'Output Management'
+         title: 'dev test'
       }),
-      new CleanWebpackPlugin(['dist'])
-   ]
-   // module: {
-   //    rules: [
-   //       {
-   //          test: /\.css$/,
-   //          use: [
-   //             'style-loader',
-   //             'css-loader'
-   //          ]
-   //       },
-   //       {
-   //          test: /\.(png|svg|jpg|gif)$/,
-   //          use: [
-   //             'file-loader'
-   //          ]
-   //       },
-   //       {
-   //          test: /\.(woff|woff2|eot|ttf|otf)$/,
-   //          use: [
-   //             'file-loader'
-   //          ]
-   //       },
-   //       {
-   //          test: /\.(csv|tsv)$/,
-   //          use: [
-   //             'csv-loader'
-   //          ]
-   //       },
-   //       {
-   //          test: /\.xml$/,
-   //          use: [
-   //             'xml-loader'
-   //          ]
-   //       }
-   //    ]
-   // },
+      new CleanWebpackPlugin(['dist']),
+      new webpack.NamedChunksPlugin(),
+      new webpack.HotModuleReplacementPlugin()
+   ],
+   module: {
+      rules: [
+         {
+            test: /\.css$/,
+            use: [
+               'style-loader',
+               'css-loader'
+            ]
+         },
+         {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: [
+               'file-loader'
+            ]
+         },
+         {
+            test: /\.(woff|woff2|eot|ttf|otf)$/,
+            use: [
+               'file-loader'
+            ]
+         },
+         {
+            test: /\.(csv|tsv)$/,
+            use: [
+               'csv-loader'
+            ]
+         },
+         {
+            test: /\.xml$/,
+            use: [
+               'xml-loader'
+            ]
+         }
+      ]
+   },
 };
